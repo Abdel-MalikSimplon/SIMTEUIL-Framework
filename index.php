64decdecba5809303be79ff409d17cdea988f129
Abdel-Malik <Ushiden13@gmail.com>
2016-05-27 02:04:19 +0200

    add index.php and styles.css

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <title>SIMTUEIL Framework</title>
+        <link rel="stylesheet" href="styles.css" />
+    </head>
+    <body>
+        <div class="container containerRow">
+            <div class="bloc bgColorRed ajoutContainer containerColumn">
+                <div class="bloc bgColorYellow">
+
+                </div>
+                <div class="bloc bgColorGreen">
+
+                </div>
+            </div>
+            <div class="bloc bgColorOrange">
+
+            </div>
+        </div>
+
+        <div class="container containerRow">
+            <div class="bloc bgColorBlack">
+
+            </div>
+            <div class="bloc bgColorYellow">
+
+            </div>
+        </div>
+    </body>
+</html>
